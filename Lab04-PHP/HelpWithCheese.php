e0e78026daf50cb2148c6700b59424e07d4cc797
DESKTOP-IOQA2SJ\nickh <nickharle@gmail.com>
2019-02-09 14:57:05 +0000

    Lab04 object page generation responsive form added

--- Lab04-PHP/HelpWithCheese.php
+++ Lab04-PHP/HelpWithCheese.php
@@ -15,35 +15,59 @@ if (!isset($HelpWithCheese)) {
 $HelpWithCheese->content = '
 
             <h2>Help With Cheese</h2>
+            <div class="container">
                 <form action="#" method="post" id="formlayout">
                 
-                    <!-- Labels -->
-                    <section class="grid-20">
-                        <label for="name">Name:</label>
-                        <br><br>
-                        <label for="email">Email:</label>
-                        <br><br>
-                        <label for="favcheese">Favourite Cheese</label>
-                        <br><br>
-                        <label for="problem">What is your cheese problem?</label>
-                    </section>
+                    <!-- NAME -->
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="name"><p>Name:</p></label>
+                        </div>
+                        <div class="col-75">
+                            <input type="text" name="name" size="25" maxlength="20" required/>
+                        </div>
+                    </div>
                     
-                    <!-- Input fields -->
-                    <section class="grid-80">
-                        <input type="text" name="name" size="25" maxlength="20" required/>
-                        <br><br>
-                        <input type="email" name="email" size="25" maxlength="20" required/>
-                        <br><br>
-                        <input type="radio" name="favcheese" value="feta" />
-                        Feta
-                        <input type="radio" name="favcheese" value="gouda" />
-                        Gouda
-                        <br><br>
-                        <textarea name="problem", rows="10" cols="50">Enter your cheese problem here
-                        </textarea>
-                    </section>
-                    <input type="submit" value="Submit"/>
-                </form>';
+                    <!-- EMAIL -->
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="email"><p>Email:</p></label>
+                        </div>
+                        <div class="col-75">
+                            <input type="email" name="email" size="25" maxlength="20" required/>
+                        </div>
+                    </div>
+                    
+                    <!-- FAVOURITE CHEESE -->
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="favcheese"><p>Favourite Cheese</p></label>
+                        </div>
+                        <div class="col-75">
+                            <input type="radio" name="favcheese" value="feta" />
+                            <span id="radio">Feta</span>
+                            <input type="radio" name="favcheese" value="gouda" />
+                            <span id="radio">Gouda</span>
+                        </div>
+                    </div>
+                    
+                    <!-- CHEESE PROBLEM -->
+                    <div class="row">
+                        <div class="col-25">
+                            <label for="problem"><p>What is your cheese problem?</p></label>
+                        </div>
+                        <div class="col-75">
+                            <textarea name="problem", rows="10" cols="50">Enter your cheese problem here
+                            </textarea>
+                        </div>
+                    </div>
+                    
+                    <!-- SUBMIT -->
+                    <div class="row">
+                        <input type="submit" value="Submit"/>
+                    </div>
+                </form>
+            </div>';
 
 $HelpWithCheese->title ="Help With Cheese";
 
