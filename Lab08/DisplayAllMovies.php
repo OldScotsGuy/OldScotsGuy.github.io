028afef1da98b423133df1c7bd536c90a5ed21be
OldScotsGuy <nickharle@gmail.com>
2019-02-25 11:44:22 +0000

    Lab08 completed

---
+++ Lab08/DisplayAllMovies.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1813014
+ * Date: 25/02/2019
+ * Time: 10:18
+ */
+include("dbConnect.php");
+
+$sql_query = "SELECT * FROM marvelmovies ";
+$result = $db->query($sql_query);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Read Marvel Database</title>
+</head>
+<body>
+<h2>Marvel Database Contents</h2>
+<p>Table contents</p>
+<table>
+    <thead>
+    <tr><th>Year</th><th>Title</th><th>Studio</th><th>Notes</th></tr>
+    </thead>
+    <tbody>
+    <?php
+    while ($row = $result->fetch_array()) {
+        $output = "<tr><td>" . $row['yearReleased'] . "</td><td>" . $row['title'] . "</td>";
+        $output .= "<td>" . $row['productionStudio'] . "</td><td>" . $row['notes'] . "</td></tr>";
+        echo $output;
+    }
+
+    ?>
+    </tbody>
+</table>
+</body>
+</html>
+<?php
+$result->close();
+$db->close;
---
+++ Lab08/dbConnect.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1813014
+ * Date: 25/02/2019
+ * Time: 10:20
+ */
+
+define('DB_SERVER', 'CSDM-WEBDEV');
+define('DB_USERNAME', '1813014');
+define('DB_PASSWORD', '1813014');
+define('DB_DATABASE', 'db1813014_cmm007');
+
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
